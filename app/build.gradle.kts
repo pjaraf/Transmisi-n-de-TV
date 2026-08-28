debd6c2f718c76b54858f29b44e6d3535b38f1a3
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-08-28 06:08:36 +0200

    ac

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.tvstream.jxmldq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = (project.findProperty("versionCode") as? String)?.toInt() ?: 1
+    versionName = (project.findProperty("versionName") as? String) ?: "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
